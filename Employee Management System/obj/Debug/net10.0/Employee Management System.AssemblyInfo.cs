540eb7329c618ee2a006c4a9ec39e80de7fb2b85
iam-knowledge <owelelwazi1@gamail.com>
2026-01-21 16:32:45 +0200

    Modified the sharedlayout back

--- Employee Management System/obj/Debug/net10.0/Employee Management System.AssemblyInfo.cs
+++ Employee Management System/obj/Debug/net10.0/Employee Management System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Employee Management System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24014a569714c701eca7c1b2e7dcf7f895f2cc7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbc4c60fa83f25435236c046be54ca8e52c0e521")]
 [assembly: System.Reflection.AssemblyProductAttribute("Employee Management System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Employee Management System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
